d87145816b3c581f576f46e5c2bf9f7d461a5088
Dilène <129503855+dilenesantos@users.noreply.github.com>
2024-12-17 12:20:47 +0100

    Update bank_app.py

--- dilenesantos/bank_app.py
+++ dilenesantos/bank_app.py
@@ -266,14 +266,7 @@ X_train_sd = pd.concat([X_train_sd.drop('weekday', axis=1), dummies_sd], axis=1)
 dummies_sd = pd.get_dummies(X_test_sd['weekday'], prefix='weekday').astype(int)
 X_test_sd = pd.concat([X_test_sd.drop('weekday', axis=1), dummies_sd], axis=1)
     
-@st.cache_resource
-def load_model():
-    return joblib.load("dilenesantos/random_forest_model_carolle.pkl")
-            
-loaded_model_carolle = load_model()
 
-#appel des shap values du modèle sauvegardé rf_carolle
-shap_values_carolle = joblib.load("dilenesantos/shap_values_carolle.pkl")
 
 with st.sidebar:
     selected = option_menu(
@@ -1742,10 +1735,6 @@ if selected == 'Interprétation':
             
             # Affichage des visualisations SHAP
             st.subheader("Summary plot")
-
-            fig = plt.figure()
-            shap.summary_plot(shap_values_carolle[:,:,1], X_test)  # Supposant un problème de classification binaire
-            st.pyplot(fig)
 
         if submenu_interpretation == "Bar plot poids des variables" :
             st.subheader("Poids des variables dans le modèle")
