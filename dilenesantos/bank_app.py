e8aff8eecdf372b5f712139a727c42889a01f5cd
Dilène <129503855+dilenesantos@users.noreply.github.com>
2024-12-18 23:38:16 +0100

    Update bank_app.py

--- dilenesantos/bank_app.py
+++ dilenesantos/bank_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 import scipy.stats as stats
 from sklearn.model_selection import train_test_split
