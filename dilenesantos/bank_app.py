175254822a3b788f619c56818bc4ec7f440a1362
Dilène <129503855+dilenesantos@users.noreply.github.com>
2024-12-22 23:11:53 +0100

    Update bank_app.py

--- dilenesantos/bank_app.py
+++ dilenesantos/bank_app.py
@@ -4126,27 +4126,8 @@ if selected == 'PRED POUSSÉ':
         
     dff_TEST_poutcome = dff_TEST_poutcome.drop(['deposit'], axis=1)
 
-    X_train_o_poutcome['poutcome'] = X_train_o_poutcome['poutcome'].fillna(method ='bfill')
-    X_train_o_poutcome['poutcome'] = X_train_o_poutcome['poutcome'].fillna(X_train_o_poutcome['poutcome'].mode()[0])
-
-    X_test_o_poutcome['poutcome'] = X_test_o_poutcome['poutcome'].fillna(method ='bfill')
-    X_test_o_poutcome['poutcome'] = X_test_o_poutcome['poutcome'].fillna(X_test_o_poutcome['poutcome'].mode()[0]) 
-    
-    # Séparation des données en un jeu d'entrainement et jeu de test
-    X_train_o_poutcome, X_test_o_poutcome, y_train_o_poutcome, y_test_o_poutcome = train_test_split(X_dff_TEST_poutcome, y_dff_TEST_poutcome, test_size = 0.20, random_state = 48)
-                        
-    # On fait de même pour les NaaN de 'education'
-    X_train_o_poutcome['education'] = X_train_o_poutcome['education'].fillna(method ='bfill')
-    X_train_o_poutcome['education'] = X_train_o_poutcome['education'].fillna(X_train_o_poutcome['education'].mode()[0])
-    
-    X_test_o_poutcome['education'] = X_test_o_poutcome['education'].fillna(method ='bfill')
-    X_test_o_poutcome['education'] = X_test_o_poutcome['education'].fillna(X_test_o_poutcome['education'].mode()[0])
-    
-    X_train_o_poutcome['poutcome'] = X_train_o_poutcome['poutcome'].fillna(method ='bfill')
-    X_train_o_poutcome['poutcome'] = X_train_o_poutcome['poutcome'].fillna(X_train_o_poutcome['poutcome'].mode()[0])
-    
-    X_test_o_poutcome['poutcome'] = X_test_o_poutcome['poutcome'].fillna(method ='bfill')
-    X_test_o_poutcome['poutcome'] = X_test_o_poutcome['poutcome'].fillna(X_test_o_poutcome['poutcome'].mode()[0])    
+    dff_TEST_poutcome['poutcome'] = dff_TEST_poutcome['poutcome'].fillna(method ='bfill')
+    dff_TEST_poutcome['poutcome'] = dff_TEST_poutcome['poutcome'].fillna(dff_TEST_poutcome['poutcome'].mode()[0])    
                 
     # Standardisation des variables quantitatives:
     scaler_o = StandardScaler()
