574f17bda81eed9f64eff42ceb0c42d51399fafa
DF1117 <95486286+DF1117@users.noreply.github.com>
2024-12-27 00:39:16 +0100

    Update bank_app.py

--- dilenesantos/bank_app.py
+++ dilenesantos/bank_app.py
@@ -2056,7 +2056,7 @@ if selected == 'Interprétation':
                 st.write("#### DURATION : Poids de +0.19 dans les prédictions de notre modèle")  
                 st.subheader("IMPACT POSITIF DE DURATION SUR LA CLASSE 1")
                 st.write("Summary plot :")
-                st.write("Shape du shap values du modèle sélectionné :", shap_values_RF_carolle.shape)
+                #st.write("Shape du shap values du modèle sélectionné :", shap_values_RF_carolle.shape)
                 
                 fig = plt.figure()
                 shap.summary_plot(shap_values_RF_carolle[:, [X_test.columns.get_loc("duration")]], 
