bee611333282d1d239edc5c5cb7e7a19cd143fa5
Dilène <129503855+dilenesantos@users.noreply.github.com>
2024-12-28 17:42:17 +0100

    Update bank_app.py

--- dilenesantos/bank_app.py
+++ dilenesantos/bank_app.py
@@ -2811,41 +2811,7 @@ if selected == 'Interprétation':
                 ax.axhline(0, color='red', linewidth=1.5, linestyle='--')
                 st.pyplot(fig)       
                 plt.close()
-
-
-                st.write("Test")
-
-                feature_name = "age"
-                
-                # Tracer le graphique de dépendance SHAP
-                fig, ax = plt.subplots(figsize=(20, 7))
-                
-                # Générer le graphique de dépendance avec les vraies valeurs
-                shap.dependence_plot(feature_name, 
-                                      shap_values=shap_XGBOOST_1_VALUES, 
-                                      features=X_test_original_figures, 
-                                      interaction_index=feature_name, 
-                                      show=False)
-                
-                # Ajouter une ligne horizontale à zéro
-                plt.axhline(0, color='red', linestyle='--', linewidth=1)
-                
-                # Configurez les limites et les ticks de l'axe x
-                ax.set_xlim(17, 76)  # Limites des âges
-                ax.set_xticks(np.arange(17, 77, 1))  # Ticks pour chaque année
-                
-                plt.xlabel("Âge")
-                plt.ylabel("Valeurs SHAP")
-                plt.title("Graphique de Dépendance des Valeurs SHAP")
-                
-                # Afficher le graphique dans Streamlit
-                st.pyplot(fig)
-                plt.close()
-                # Afficher le graphique dans Streamlit
-                st.pyplot(fig)
-                plt.close()
-
-                
+     
                 st.write("blabla") 
 
 
