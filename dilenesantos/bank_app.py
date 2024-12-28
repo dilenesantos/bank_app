4d994eec64e9b78342d6099e0b07abfea7454984
Dilène <129503855+dilenesantos@users.noreply.github.com>
2024-12-28 11:37:32 +0100

    Update bank_app.py

--- dilenesantos/bank_app.py
+++ dilenesantos/bank_app.py
@@ -768,7 +768,7 @@ if selected == 'DataVisualisation':
 
     if pages == "Profiling" :  
         # Title and Introduction
-        st.title("Profil des clients ayant souscrit à un dépôt à terme")
+        st.subheader("Profil des clients ayant souscrit à un dépôt à terme")
         
         # Filter the dataset
         dff = df[df['job'] != "unknown"]  # Remove rows with unknown job
